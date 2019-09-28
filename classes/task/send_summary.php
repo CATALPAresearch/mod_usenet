7d6944fed81283c1480d6b7771f79dbc3db44720
Rudolf Patzer <rpatzer@gmx.de>
2019-09-28 18:15:23 +0200

    beautify

--- classes/task/send_summary.php
+++ classes/task/send_summary.php
@@ -5,54 +5,55 @@ namespace mod_newsmod\task;
 /**
  * An example of a scheduled task.
  */
-class send_summary extends \core\task\scheduled_task {
+class send_summary extends \core\task\scheduled_task
+{
 
     /**
      * Return the task's name as shown in admin screens.
      *
      * @return string
      */
-    public function get_name() {
+    public function get_name()
+    {
         return get_string('email_send', 'newsmod');
     }
 
     /**
      * Execute the task.
      */
-    public function execute() {
+    public function execute()
+    {
         // Apply fungus cream.
         // Apply chainsaw.
         // Apply olive oil.
-	global $CFG,$DB;
+        global $CFG,$DB;
 
-	require_once($CFG->dirroot . '/mod/newsmod/libconn.php');
-	$summary = $DB->get_records('user');
-	$fp = fopen('./crontest.txt', 'w');
-	$cachesummary = array();
+        require_once($CFG->dirroot . '/mod/newsmod/libconn.php');
+        $summary = $DB->get_records('user');
+        $fp = fopen('./crontest.txt', 'w');
+        $cachesummary = array();
         $httppos = $DB->get_records_sql("
                 SELECT wwwroot
                 FROM mdl_mnet_host
                 WHERE id = '1';
                 ");
-$lastruntime = $DB->get_record_sql("
+        $lastruntime = $DB->get_record_sql("
                 SELECT t.lastruntime
                 FROM {task_scheduled} t
                 where t.component = 'mod_newsmod'");
 
-        if($lastruntime->lastruntime>0){
-                $timetosearch=time();
+        if ($lastruntime->lastruntime>0) {
+            $timetosearch=time();
+        } else {
+            $timetosearch=strtotime("-40 hours", $lastruntrime->lastruntime);
         }
-        else{
-                $timetosearch=strtotime("-40 hours", $lastruntrime->lastruntime);
+
+        foreach ($httppos as $key) {
+            $httppos=$key->wwwroot;
         }
 
-foreach ($httppos as $key) {
-$httppos=$key->wwwroot;
-}
-
-	foreach ($summary as $record){
-
-		$enrolled = $DB->get_records_sql("
+        foreach ($summary as $record) {
+            $enrolled = $DB->get_records_sql("
 		SELECT nm.newsgroup, u.id ,u.email, u.firstname
 		FROM {course} c
 		JOIN {newsmod} nm ON nm.course = c.id
@@ -61,8 +62,8 @@ $httppos=$key->wwwroot;
 		JOIN {user} u ON u.id = ra.userid
 		JOIN {role} r ON r.id = ra.roleid
 		where u.id = $record->id");
-$summarytext = '
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+            $summarytext = '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
                       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -72,55 +73,54 @@ $summarytext = '
 </head>
 <body style="margin: 0; padding: 0;">';
 
-		$summarytext = $summarytext . "<b>Guten Tag, diese E-Mail enthält die tägliche Zusammenfassung neuer Newsgroupbeiträge seit dem ".date("d-M-Y",$lastruntime->lastruntime)."</b>";
-$content= "";
-		foreach ($enrolled as $newsgr){
-		      $testhallo= $newsgr->newsgroup;
-                      $enrolledd = $DB->get_records_sql("
+            $summarytext = $summarytext . "<b>Guten Tag, diese E-Mail enthält die tägliche Zusammenfassung neuer Newsgroupbeiträge seit dem ".date("d-M-Y", $lastruntime->lastruntime)."</b>";
+            $content= "";
+            foreach ($enrolled as $newsgr) {
+                $testhallo= $newsgr->newsgroup;
+                $enrolledd = $DB->get_records_sql("
                       SELECT cm.id, nm.newsgroup FROM {modules} c
                       JOIN {course_modules} cm ON cm.module = c.id
                       JOIN {newsmod} nm ON nm.id = cm.instance
                       WHERE c.name = 'newsmod'");
-			$key = array_search($newsgr->newsgroup, array_column($enrolledd, 'newsgroup'));
-			$keys = array_slice($enrolledd,$key,1);
+                $key = array_search($newsgr->newsgroup, array_column($enrolledd, 'newsgroup'));
+                $keys = array_slice($enrolledd, $key, 1);
 
-		if(count($enrolled)>0){
-			$content =1;
-			}
-			if(!isset($cachesummary[$newsgr->newsgroup])){
-			$cachesummary[$newsgr->newsgroup]=summary($newsgr, $timetosearch);
-			}
-			if(count($cachesummary[$newsgr->newsgroup])>0){
-			//$summarytext = $summarytext . "\r\n\r\n<hr size=1 noshade=noshade />";
-			$summarytext = $summarytext ."<h3>Newsgruppe: ". $newsgr->newsgroup."</h3>\r\n";
-          		$summarytext = $summarytext . '<table border="0" height="200" cellpadding="0" cellspacing="0" width="100%">';
-		}
+                if (count($enrolled)>0) {
+                    $content =1;
+                }
+                if (!isset($cachesummary[$newsgr->newsgroup])) {
+                    $cachesummary[$newsgr->newsgroup]=summary($newsgr, $timetosearch);
+                }
+                if (count($cachesummary[$newsgr->newsgroup])>0) {
+                    //$summarytext = $summarytext . "\r\n\r\n<hr size=1 noshade=noshade />";
+                    $summarytext = $summarytext ."<h3>Newsgruppe: ". $newsgr->newsgroup."</h3>\r\n";
+                    $summarytext = $summarytext . '<table border="0" height="200" cellpadding="0" cellspacing="0" width="100%">';
+                }
 
 
-		foreach($cachesummary[$newsgr->newsgroup] as $message){
-		$summarytext= $summarytext .'<tr><td height="10px">';
-                $summarytext = $summarytext .  '<a class="searcher" href="'.$httppos .'/mod/newsmod/view.php?id='. $keys[0]->id .'&msgnr='. $message->uid .'">';
-                //$summarytext = $summarytext . '<li class="node" style="list-style:none">';
-                //$summarytext = $summarytext .  '<svg width="100" height="100" data-jdenticon-value=">';
-                $tempheader->sender=imap_rfc822_parse_adrlist($message->from,'');
-                //print_r(new moodle_url("/mod/newsmod/messageid.php?id=" . $newsgr->id . "&msgnr=" . $message->uid));
-                $summarytext = $summarytext . '<p>Betreff: '. htmlspecialchars($message->subject).'</p><p> Absender: ';
-                $summarytext = $summarytext . $tempheader->sender[0]->mailbox."@".$tempheader->sender[0]->host;
-                $summarytext = $summarytext . $tempheader->personal.'</p>';
-                //$summarytext = $summarytext . '</div><div class="timelist" timestamp="' .$message->date.'"';
-                //$summarytext = $summarytext . '</div></div></div></li></a>';
-		$summarytext = $summarytext .'</td></tr>';
-			}
-		$summarytext = $summarytext .'</table>';
-		}
+                foreach ($cachesummary[$newsgr->newsgroup] as $message) {
+                    $summarytext= $summarytext .'<tr><td height="10px">';
+                    $summarytext = $summarytext .  '<a class="searcher" href="'.$httppos .'/mod/newsmod/view.php?id='. $keys[0]->id .'&msgnr='. $message->uid .'">';
+                    //$summarytext = $summarytext . '<li class="node" style="list-style:none">';
+                    //$summarytext = $summarytext .  '<svg width="100" height="100" data-jdenticon-value=">';
+                    $tempheader->sender=imap_rfc822_parse_adrlist($message->from, '');
+                    //print_r(new moodle_url("/mod/newsmod/messageid.php?id=" . $newsgr->id . "&msgnr=" . $message->uid));
+                    $summarytext = $summarytext . '<p>Betreff: '. htmlspecialchars($message->subject).'</p><p> Absender: ';
+                    $summarytext = $summarytext . $tempheader->sender[0]->mailbox."@".$tempheader->sender[0]->host;
+                    $summarytext = $summarytext . $tempheader->personal.'</p>';
+                    //$summarytext = $summarytext . '</div><div class="timelist" timestamp="' .$message->date.'"';
+                    //$summarytext = $summarytext . '</div></div></div></li></a>';
+                    $summarytext = $summarytext .'</td></tr>';
+                }
+                $summarytext = $summarytext .'</table>';
+            }
 
-		$summarytext = $summarytext . "</html>";
-if($content>0){
-		sendemail($record->id, $summarytext);
-		$content=0;
-}
-
-}
-		fclose($fp);
-      }
+            $summarytext = $summarytext . "</html>";
+            if ($content>0) {
+                sendemail($record->id, $summarytext);
+                $content=0;
+            }
+        }
+        fclose($fp);
+    }
 }
