e40a724b7ae27145b7a21d9edb8defb4d7e0c46a
friedrich <konstantfriedrich@gmail.com>
2020-04-18 14:46:00 +0200

    added .eslintrc

---
+++ amd/src/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    rules: {
+        'no-console': 'off',
+        'no-unused-vars': 'off',
+        'max-len': 'off',
+        'no-trailing-spaces': 'off',
+        'no-tabs': 'off',
+        'no-return-assign': 'off'
+    }
+};
