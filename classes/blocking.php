ddf8fbf364ac0dc4ab85af8fed3d53113bddff55
nise <niels.seidel@nise81.com>
2023-04-19 08:32:00 +0200

    fixed policy version number

--- classes/blocking.php
+++ classes/blocking.php
@@ -22,8 +22,8 @@ defined('MOODLE_INTERNAL') || die();
 
 class blocking
 {
-    const policy_version = 3; // local_niels: 11  aple: 3 marc: 1
-    const disable_blocking = true;
+    const policy_version = 6; // local_niels: 11  aple: 3 marc: 1
+    const disable_blocking = false;
     const disable_whitelist = false;
     const whitelist = array(
         '127.0.0.1',
